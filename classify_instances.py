044c3de10bf0549b888740fe0e64b8301bcb99c3
Rik <rikvannoord@gmail.com>
2016-05-02 14:55:06 +0200

    ok

--- classify_instances.py
+++ classify_instances.py
@@ -296,6 +296,7 @@ test = MultinomialNB()
 pred = cross_validation_own(word_array, labels, num_folds, down_sample, test, False) ## first do only words, don't print
 print pred
 print 'len pred is',len(pred)
+print len(other_array[0])
 clf_array = add_clf_features(other_array, pred)
 cross_validation_own(clf_array, labels, num_folds, down_sample, test, print_res)
 
