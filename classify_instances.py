5c5216fd832284d1e0109d59ce2df853f97613fb
Rik <rikvannoord@gmail.com>
2016-05-03 14:40:08 +0200

    ok

--- classify_instances.py
+++ classify_instances.py
@@ -276,8 +276,8 @@ down_sample = False
 num_folds = 5
 num_jobs = 16 			## for svm number of parallel jobs
 
-array = numpy.load(labeled_data)
-array = array.todense()											## get dense matrix from sparse dok matrix
+array_dok = numpy.load(labeled_data)
+array = array_dok.todense()											## get dense matrix from sparse dok matrix
 
 print array.shape
 	
